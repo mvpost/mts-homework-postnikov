4e656d2e1deb79fe82672ef0339183831db0ee1d
postnikov <mvpost@bk.ru>
2024-03-25 07:54:49 +0300

    new branch

--- lesson2/src/lesson2.java
+++ lesson2/src/lesson2.java
@@ -5,5 +5,7 @@ import java.util.Arrays;
 public class lesson2 {
     public static void main(String[] args) {
         System.out.println("lesson2" + Arrays.deepToString(args));
+
+        System.out.println("bugfix-branch");
     }
 }
